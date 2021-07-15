ac832c25a5cec40833ef3e67d071989ce8cb11a1
Widsky <76448531+Widsky@users.noreply.github.com>
2021-07-15 18:06:25 +0200

    add form validation

--- development/js/Income.js
+++ development/js/Income.js
@@ -1,12 +1,20 @@
 import React, {useState} from "react";
 import "../scss/main.scss"
 
+import validator from 'validator';
+
 import {Expenses} from "./Expenses";
 
 export function Income() {
     const [summary, setSummary] = useState(0);
+    const [error, setError] = useState(false);
 
     const handleChange = () => {
+        setError(false);
+
+        if(validator.isEmpty(document.querySelectorAll('.income__input')[0].value) || validator.isEmpty(document.querySelectorAll('.income__input')[1].value)){
+            return setError(true);
+        }
         let a = parseInt(document.querySelectorAll('.income__input')[0].value);
         let b = parseInt(document.querySelectorAll('.income__input')[1].value);
 
@@ -22,14 +30,15 @@ export function Income() {
                 <form className="income__column__container">
                     <div className="income__column">
                         <span className="income__column__title">Kwota wypłaty</span>
-                        <input type="number" className="income__input" min="0" max="1000000000000000000000000" placeholder="Kwota netto" onInput={handleChange} defaultValue={0}/>
+                        <input type="number" className="income__input" min="0" placeholder="Kwota netto" onInput={handleChange} defaultValue={0}/>
                     </div>
 
                     <div className="income__column">
                         <span className="income__column__title">Pozostałości z poprzedniego miesiąca</span>
-                        <input type="number" className="income__input" min="0" max="1000000000000000000000000" placeholder="Kwota netto" onInput={handleChange} defaultValue={0}/>
+                        <input type="number" className="income__input" min="0" placeholder="Kwota netto" onInput={handleChange} defaultValue={0}/>
                     </div>
                 </form>
+                {(error ? <p className="error">Wpisz wartość do obu pól!</p> : false)}
             </section>
 
             <Expenses summary={summary}/>
